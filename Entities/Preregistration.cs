415d8529093d8a03f92db4295d838eaffc08d968
JuanRangel-FGJTam <143844965+JuanRangel-FGJTam@users.noreply.github.com>
2024-04-19 13:40:43 -0600

    👌 IMPROVE: Hidden password when return the data

--- Entities/Preregistration.cs
+++ Entities/Preregistration.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace AuthApi.Entities
 {
@@ -11,7 +12,8 @@ namespace AuthApi.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id {get;set;} = Guid.NewGuid();
         public string? Mail {get;set;} 
-        public string? Phone {get;set;} 
+        public string? Phone {get;set;}
+        [JsonIgnore]
         public string? Password {get;set;}
         public DateTime CreatedAt {get;set;} = DateTime.Now;
         public DateTime ValidTo {get;set;} = DateTime.Now.AddDays(2);
