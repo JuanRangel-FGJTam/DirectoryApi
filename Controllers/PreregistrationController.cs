7d9d746f0a9284b6f9b4a3d755572982231aae14
JuanRangel-FGJTam <143844965+JuanRangel-FGJTam@users.noreply.github.com>
2024-04-19 16:02:01 -0600

    📖 DOC: Add swagger docs

--- Controllers/PreregistrationController.cs
+++ Controllers/PreregistrationController.cs
@@ -28,6 +28,9 @@ namespace AuthApi.Controllers
 
         /// <summary>
         /// </summary>
+        /// <response code="201">Succsessfull create the pre-register record</response>
+        /// <response code="400">The request is not valid ore some error are present</response>
+        /// <response code="422">Some request params are not valid</response>
         [HttpPost]
         public IActionResult RegisterUser( PreregistrationRequest request )
         {
@@ -35,7 +38,7 @@ namespace AuthApi.Controllers
             // Validate request
             if (!ModelState.IsValid)
             {
-                return BadRequest();
+                return UnprocessableEntity( ModelState );
             }
     
             // Create the pre-register record
@@ -56,6 +59,7 @@ namespace AuthApi.Controllers
         }
 
         /// <summary>
+        /// Retrive all the preregisters stored
         /// </summary>
         [HttpGet]
         public ActionResult<IEnumerable<Preregistration>?> GetAllPreregisters()
@@ -63,7 +67,28 @@ namespace AuthApi.Controllers
             return Ok( dbContext.Preregistrations.ToArray() );
         }
 
-        
+        /// <summary>
+        /// Store the new person using the pre-register record for retriving the email and password
+        /// </summary>
+        /// <remarks>
+        /// Sample request with the minal data required:
+        /// 
+        ///     POST api/people
+        ///     {
+        ///       "curp": "RAAE190394MTSNLL02",
+        ///       "name": "Juan Salvador",
+        ///       "firstName": "Rangel",
+        ///       "lastName": "Almaguer",
+        ///       "birthdate": "1993-12-17"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="preregisterID"></param>
+        /// <param name="request"></param>
+        /// <response code="201">Succsessfull stored the person</response>
+        /// <response code="400">The request is not valid or are parameters missing</response>
+        /// <response code="404">The preregister record was not found</response>
+        /// <response code="422">Some request params are not valid</response>
         [Route("{preregisterID}")]
         [HttpPost]
         public IActionResult ValidateRegister(Guid preregisterID, [FromBody] ValidateRegisterRequest request){
@@ -72,14 +97,14 @@ namespace AuthApi.Controllers
                 // Retrive validation enity
                 var preregister = this.dbContext.Preregistrations.Find(preregisterID);
                 if( preregister == null){
-                    return BadRequest(new {
+                    return NotFound(new {
                         message = "Preregister record was not found on the database"
                     });
                 }
 
                 var newPerson = this.preregisterService.ValidateRegister( preregister.Id, request );    
                 if( newPerson != null){
-                    return Ok( new {
+                    return Created("", new {
                         personId = newPerson.Id.ToString(),
                         fullName = newPerson.FullName
                     });
