091732a8e2637a52287e5f435f43553c7c5eb78e
JuanRangel-FGJTam <143844965+JuanRangel-FGJTam@users.noreply.github.com>
2024-05-08 16:37:14 -0600

    📦 NEW: Creted the migration for update the preregister table

---
+++ Migrations/20240508212537_updated_preregister_table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updated_preregister_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "Preregistrations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updatedAt",
+                table: "Preregistrations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "Preregistrations");
+
+            migrationBuilder.DropColumn(
+                name: "updatedAt",
+                table: "Preregistrations");
+        }
+    }
+}
