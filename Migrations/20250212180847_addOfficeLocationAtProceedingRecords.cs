2df14f7329291afa2d45354503a7f1b12b6320f3
J. Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2025-02-12 12:11:22 -0600

    Modified the schema of the Procedding record

---
+++ Migrations/20250212180847_addOfficeLocationAtProceedingRecords.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOfficeLocationAtProceedingRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "officeLocation",
+                table: "Proceedings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "officeLocation",
+                table: "Proceedings");
+        }
+    }
+}
