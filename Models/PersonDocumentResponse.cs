a996e52bc5eaadad587a982f414df54fa16bcf40
Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2025-03-05 16:24:55 -0600

    Implemented stored and retrive the back of the document.

--- Models/PersonDocumentResponse.cs
+++ Models/PersonDocumentResponse.cs
@@ -13,9 +13,12 @@ namespace AuthApi.Models
         public Guid PersonId {get;set;}
         public string? FileName {get;set;}
         public string? FilePath {get;set;}
+        public string? FilePathBack {get;set;}
         public string? MimmeType {get;set;}
         public string? FileUrl {get;set;}
+        public string? FileUrlBack {get;set;}
         public long FileSize {get;set;}
+        public long? FileSizeBack {get;set;}
         public int DocumentTypeId {get;set;}
         public string? DocumentTypeName {get;set;}
         public string? Valid {get;set;}
@@ -29,8 +32,10 @@ namespace AuthApi.Models
             response.Id = pf.Id;
             response.FileName = pf.FileName;
             response.FilePath = pf.FilePath;
+            response.FilePathBack = pf.FilePathBack;
             response.MimmeType = pf.MimmeType;
             response.FileSize = pf.FileSize;
+            response.FileSizeBack = pf.FileSizeBack;
             response.CreatedAt = pf.CreatedAt;
             response.DeletedAt = pf.DeletedAt;
             response.DocumentTypeId = pf.DocumentType!.Id;
@@ -38,6 +43,7 @@ namespace AuthApi.Models
             response.Valid = pf.Validation.ToString("yyyy-MM-dd");
             response.Folio = pf.Folio;
             response.FileUrl = null;
+            response.FileUrlBack = null;
             return response;
         }
 
