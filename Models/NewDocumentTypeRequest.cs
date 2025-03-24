57a4673d3f69d437a6b4d34192a3b4aeb814e543
Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2025-03-24 11:52:23 -0600

    Created endpoints for add, update and delete document types

--- Models/NewDocumentTypeRequest.cs
+++ Models/NewDocumentTypeRequest.cs
@@ -1,10 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AuthApi.Models
 {
     public class NewDocumentTypeRequest
     {
-
+        [Required]
         public string? Name {get; set;}
 
     }
